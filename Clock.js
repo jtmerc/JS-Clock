fbe575ce4f3de0611149e37e7b2d0d2f3e83fbaa
jadentm <jadentm@hotmail.com>
2022-07-20 16:45:54 -0500

    CLOCK

---
+++ Clock.js
@@ -0,0 +1,30 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotat', rotationRatio * 360)
+}
+
+// Current date functions
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
